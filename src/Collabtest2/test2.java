e211b7ad59e56902b92fbdcf151600e48537f6fc
bobby <ande970s@stud.kea.dk>
2022-03-11 10:28:08 +0100

    Commit test

--- src/Collabtest2/test2.java
+++ src/Collabtest2/test2.java
@@ -3,5 +3,10 @@ package Collabtest2;
 public class test2 {
   public static void main(String[] args) {
     System.out.println("HELLO");
+    System.out.println("Hej");
+    System.out.println("Hej");
+    System.out.println("Hej");
+    System.out.println("Hej");
+
   }
 }
